1a9e1e90d3cfd3d2622dd8361dfe122317c74204
Hugo Costa <costarafaelhugo@gmail.com>
2022-03-09 16:29:51 -0300

    Finalizei a funçao que salva dados)

--- app/src/main/java/com/hugorafaelcosta/roomdatabaseapp/NewUserActivity.kt
+++ app/src/main/java/com/hugorafaelcosta/roomdatabaseapp/NewUserActivity.kt
@@ -1,9 +1,10 @@
 package com.hugorafaelcosta.roomdatabaseapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.hugorafaelcosta.roomdatabaseapp.database.AppDatabase
 import com.hugorafaelcosta.roomdatabaseapp.database.daos.UserDao
+import com.hugorafaelcosta.roomdatabaseapp.database.models.User
 import com.hugorafaelcosta.roomdatabaseapp.databinding.ActivityNewUserBinding
 
 class NewUserActivity : AppCompatActivity() {
@@ -19,5 +20,27 @@ class NewUserActivity : AppCompatActivity() {
         this.database = AppDatabase.getInstance(this)
 
         this.userDao = this.database.userDao()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        this.binding.btnSave.setOnClickListener {
+
+
+        }
+    }
+
+    private suspend fun saveUser(firstName: String, lastName: String): Boolean {
+
+        if (firstName.isBlank() || firstName.isEmpty()) {
+            return false
+
+            this.userDao.insert(User(firstName, lastName))
+
+            return true
+
+        }
+
     }
 }
