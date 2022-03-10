605a071eaf4ddbb59bb244887122a9ef60a8573e
Hugo Costa <costarafaelhugo@gmail.com>
2022-03-10 08:53:36 -0300

    Finalizei a tela de chamar o usuario

--- app/src/main/java/com/hugorafaelcosta/roomdatabaseapp/NewUserActivity.kt
+++ app/src/main/java/com/hugorafaelcosta/roomdatabaseapp/NewUserActivity.kt
@@ -1,11 +1,16 @@
 package com.hugorafaelcosta.roomdatabaseapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.hugorafaelcosta.roomdatabaseapp.database.AppDatabase
 import com.hugorafaelcosta.roomdatabaseapp.database.daos.UserDao
 import com.hugorafaelcosta.roomdatabaseapp.database.models.User
 import com.hugorafaelcosta.roomdatabaseapp.databinding.ActivityNewUserBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class NewUserActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewUserBinding
@@ -27,20 +32,43 @@ class NewUserActivity : AppCompatActivity() {
 
         this.binding.btnSave.setOnClickListener {
 
+            CoroutineScope(Dispatchers.IO).launch {
+
+                val result = saveUser(
+                    binding.edtFirstName.text.toString(),
+                    binding.edtLastName.text.toString()
+                )
+
+                withContext(Dispatchers.Main) {
+
+                    Toast.makeText(
+                        this@NewUserActivity,
+                        if (result) "User saved" else "Error try to save user",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    if (result)
+                        finish()
+                }
+            }
 
         }
     }
 
     private suspend fun saveUser(firstName: String, lastName: String): Boolean {
 
-        if (firstName.isBlank() || firstName.isEmpty()) {
+        if (firstName.isBlank() || firstName.isEmpty())
             return false
 
-            this.userDao.insert(User(firstName, lastName))
 
-            return true
+        if (lastName.isBlank() || lastName.isEmpty())
+            return false
 
-        }
+
+        this.userDao.insert(User(firstName, lastName))
+
+        return true
 
     }
+
 }
